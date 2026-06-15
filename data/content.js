2a2aa7172ea70f4204624eb621d8381a4907c96c
Jungwoo <jwoorhee@kaist.ac.kr>
2026-06-15 16:35:43 +0900

    Merge poster/workshop filter; add poster thumbnail

--- data/content.js
+++ data/content.js
@@ -5,7 +5,8 @@
    PUBLICATION fields:
      img        thumbnail path, e.g. "images/mypaper.png" ("" = no thumbnail)
      venue      e.g. "CHI 2026"
-     oral       true = blue accent badge (oral/award), false = plain
+     type       "paper" | "poster" | "workshop"  (drives the filter tabs)
+     oral       true = blue venue badge (you gave an oral presentation)
      title      paper title
      authors    HTML string; wrap your name in <b>…</b> to bold it
      firstAuthor true if you are (co-)first author (used by the filter)
@@ -23,36 +24,40 @@ const PUBLICATIONS = [
   {
     img: "images/chi2026-wireyourway.png",
     venue: "CHI 2026",
-    oral: false,
+    type: "paper",
+    oral: true,
     title: "Wire Your Way: Hardware-Contextualized Guidance and In-situ Tests for Personalized Circuit Prototyping",
     authors: "Punn Lertjaturaphat*, <b>Jungwoo Rhee*</b>, Jaewon You, Andrea Bianchi",
     firstAuthor: true,
-    pdf: "",
-    doi: "https://dl.acm.org/doi/abs/10.1145/3772318.3791371"
+    pdf: "https://dl.acm.org/doi/pdf/10.1145/3772318.3791371",
+    doi: "https://doi.org/10.1145/3772318.3791371"
   },
   {
     img: "images/chi2026-gazeandspeech.png",
     venue: "CHI 2026",
-    oral: false,
+    type: "paper",
+    oral: true,
     title: "Gaze and Speech in Multimodal Human-Computer Interaction: A Scoping Review",
     authors: "Anam Ahmad Khan, Florian Weidner, <b>Jungwoo Rhee</b>, Yasmeen Abdrabou, Andrea Bianchi, Eduardo Velloso, Hans Gellersen, Joshua Newn",
     firstAuthor: false,
-    pdf: "",
-    doi: "https://dl.acm.org/doi/abs/10.1145/3772318.3791662"
+    pdf: "https://dl.acm.org/doi/pdf/10.1145/3772318.3791662",
+    doi: "https://doi.org/10.1145/3772318.3791662"
   },
   {
-    img: "",
-    venue: "CHI 2026 LBW",
-    oral: false,
+    img: "images/chiea26-oneisnotenough.png",
+    venue: "CHI 2026 Poster",
+    type: "poster",
+    oral: true,
     title: "One Is Not Enough: How People Use Multiple AI Models in Everyday Life",
     authors: "Seunghwa Pyo*, Donggun Lee*, <b>Jungwoo Rhee*</b>, Soobin Park, Youn-kyung Lim",
     firstAuthor: true,
-    pdf: "",
-    doi: "https://dl.acm.org/doi/abs/10.1145/3772363.3798682"
+    pdf: "https://dl.acm.org/doi/pdf/10.1145/3772363.3798682",
+    doi: "https://doi.org/10.1145/3772363.3798682"
   },
   {
     img: "",
     venue: "CHI 2026 Workshop",
+    type: "workshop",
     oral: false,
     title: "Deepening, Redirecting, Expanding: A Design Space for Generative Interfaces with Qualitative Inquiry",
     authors: "<b>Jungwoo Rhee</b>",
@@ -63,6 +68,7 @@ const PUBLICATIONS = [
   {
     img: "",
     venue: "CHI 2025 Workshop",
+    type: "workshop",
     oral: false,
     title: "Passive Observation and Asynchronous Feedback to Mitigate Meeting Fatigue",
     authors: "<b>Jungwoo Rhee</b>",
@@ -73,6 +79,7 @@ const PUBLICATIONS = [
   {
     img: "",
     venue: "HCI Korea 2024",
+    type: "paper",
     oral: false,
     title: "How Professionals Use Visual Asset Management: An Exploration through Interviews",
     authors: "<b>Jungwoo Rhee</b>, Andrea Bianchi",
@@ -86,13 +93,13 @@ const NEWS = [
   { date: "2026.06", html: "Visiting <a href='https://www.kth.se/' target='_blank' rel='noopener'>KTH Royal Institute of Technology</a> in Stockholm, Sweden for a workshop on our joint grant project.", isNew: true },
   { date: "2026.04", html: "Attended CHI 2026 in Barcelona, Spain — presented 2 papers and 1 poster, and joined 1 workshop." },
   { date: "2026.02", html: "Workshop paper accepted to the <a href='https://genuimeetshci.github.io/chi26-workshop/' target='_blank' rel='noopener'>Generative UI workshop</a> at CHI 2026: <a href='https://genuimeetshci.github.io/chi26-workshop/proposals/design_space_genui_qualitative_inquiry_chi26_workshop.pdf' target='_blank' rel='noopener'>Deepening, Redirecting, Expanding</a>." },
-  { date: "2026.02", html: "Poster (Late-Breaking Work) accepted to CHI 2026: <a href='https://dl.acm.org/doi/abs/10.1145/3772363.3798682' target='_blank' rel='noopener'>One Is Not Enough: How People Use Multiple AI Models in Everyday Life</a>." },
-  { date: "2026.01", html: "Two papers accepted to CHI 2026: <a href='https://dl.acm.org/doi/abs/10.1145/3772318.3791371' target='_blank' rel='noopener'>Wire Your Way</a> &amp; <a href='https://dl.acm.org/doi/abs/10.1145/3772318.3791662' target='_blank' rel='noopener'>Gaze and Speech in Multimodal Human-Computer Interaction</a>." },
-  { date: "2026.01", html: "Attending HCI Korea 2026 in Korea." },
+  { date: "2026.02", html: "Poster accepted to CHI 2026: <a href='https://doi.org/10.1145/3772363.3798682' target='_blank' rel='noopener'>One Is Not Enough: How People Use Multiple AI Models in Everyday Life</a>." },
+  { date: "2026.01", html: "Two papers accepted to CHI 2026: <a href='https://doi.org/10.1145/3772318.3791371' target='_blank' rel='noopener'>Wire Your Way</a> &amp; <a href='https://doi.org/10.1145/3772318.3791662' target='_blank' rel='noopener'>Gaze and Speech in Multimodal Human-Computer Interaction</a>." },
+  { date: "2026.01", html: "Attending HCI Korea 2026 in Gangwon, Korea." },
   { date: "2025.09", html: "Assistant to PC and Student Volunteer for <a href='https://uist.acm.org/2025/organizers/' target='_blank' rel='noopener'>UIST 2025</a> in Busan, Korea." },
   { date: "2025.09", html: "Started my PhD at KAIST in Daejeon, Korea." },
   { date: "2025.04", html: "Student Volunteer for CHI 2025 in Yokohama, Japan." },
-  { date: "2025.03", html: "Workshop paper accepted to the <a href='https://blendedrealities.jensemil.dk/' target='_blank' rel='noopener'>Blending Realities for Interaction across Distributed Group Environments</a> workshop at CHI 2025: <a href='https://blendedrealities.jensemil.dk/wp-content/uploads/2025/04/CHI_2025_W12_sub10_JungwooRhee.pdf' target='_blank' rel='noopener'>Passive Observation and Asynchronous Feedback to Mitigate Meeting Fatigue</a>." },
+  { date: "2025.03", html: "Workshop paper accepted to the <a href='https://blendedrealities.jensemil.dk/' target='_blank' rel='noopener'>Scaling Distributed Collaboration in Mixed Reality</a> workshop at CHI 2025: <a href='https://blendedrealities.jensemil.dk/wp-content/uploads/2025/04/CHI_2025_W12_sub10_JungwooRhee.pdf' target='_blank' rel='noopener'>Passive Observation and Asynchronous Feedback to Mitigate Meeting Fatigue</a>." },
   { date: "2025.02", html: "Attending HCI Korea 2025 in Gangwon, Korea." },
   { date: "2024.10", html: "Attending UIST 2024 in Pittsburgh, US." },
   { date: "2024.06", html: "Defended master's thesis: Supporting designers to create environmentally responsible XR UIs." },
